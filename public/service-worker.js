d426204cba19566cba5d9e03fd65d70cedca6c65
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-06-18 15:36:26 -0500

    service worker file fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,33 +2,7 @@ const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-const FILES_TO_CACHE = [
-  "./index.html",
-  "./css/styles.css",
-  "./js/index.js",
-  "./js/idb.js",
-];
-
-// Respond with cached resources
-self.addEventListener("fetch", function (e) {
-  console.log("fetch request : " + e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        // if cache is available, respond with cache
-        console.log("responding with cache : " + e.request.url);
-        return request;
-      } else {
-        // if there are no cache, try fetching request
-        console.log("file is not cached, fetching : " + e.request.url);
-        return fetch(e.request);
-      }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  );
-});
+const FILES_TO_CACHE = ["./index.html", "./css/styles.css", "./js/index.js"];
 
 // Cache resources
 self.addEventListener("install", function (e) {
@@ -63,6 +37,27 @@ self.addEventListener("activate", function (e) {
           }
         })
       );
+    })
+  );
+});
+
+// Respond with cached resources
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        // if cache is available, respond with cache
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        // if there are no cache, try fetching request
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
   );
 });
